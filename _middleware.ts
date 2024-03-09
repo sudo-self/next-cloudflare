564fc698b2a730e8b328335caace6521974d7cb3
Sudo-Self <sudo-self@proton.me>
2024-03-08 18:20:12 -0700

    Create _middleware.ts

---
+++ _middleware.ts
@@ -0,0 +1,11 @@
+export const onRequest: PagesFunction = async({request, next}) => {
+  const { url } = request;
+  const { pathname, searchParams } = new URL(url);
+  if (
+    pathname.includes("/post/") &&
+    searchParams.get("password") !== "12345678"
+  ) {
+    return new Response(null, { status: 401 });
+  }
+  return await next();
+};
